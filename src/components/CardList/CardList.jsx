84bb8df1a72bdc75c0f532c12b4238856fcc23d9
MariiaVoloshanovska <masterskaiamary@gmail.com>
2023-07-31 16:00:29 +0300

    added new update

--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -1,5 +1,5 @@
 import PropTypes from "prop-types";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { CardListWrapper, LinkBack } from "components/CardList/CardList.styled";
 import { Card } from "components/Card/Card";
 import { Filter } from "components/Filter/Filter";
@@ -14,9 +14,9 @@ export const CardList = ({ usersSet }) => {
     setFilteredUsersSet(filteredUsers);
   }, [filterValue, usersSet]);
 
-  const onChangeFilter = (e) => {
+  const onChangeFilter = useCallback((e) => {
     setFilterValue(e.target.value);
-  };
+  }, []);
 
   return (
     <>
